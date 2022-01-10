ed4773731914113fba617aa678b68b3bef6ea374
Sunjae95 <tjswo9502@gmail.com>
2022-01-10 21:56:47 +0900

    chore: 절대경로 수정

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 module.exports = {
   babel: {
     presets: ['@emotion/babel-preset-css-prop'],
+  },
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@style': path.resolve(__dirname, 'src/style'),
+    },
   },
 };
