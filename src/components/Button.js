3c1ee80154a96e3e5360b79bedde914ffeea1c2a
Sunjae95 <tjswo9502@gmail.com>
2022-01-10 17:04:18 +0900

    feat: Button컴포넌트

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const ButtonWrapper = styled.button`
+  width: ${({ buttonWidth }) => `${buttonWidth}px`};
+  height: ${({ buttonHeight }) => `${buttonHeight}px`};
+`;
+
+function Button({ children, buttonWidth, buttonHeight, ...props }) {
+  return (
+    <ButtonWrapper
+      buttonWidth={buttonWidth}
+      buttonHeight={buttonHeight}
+      {...props}
+    >
+      {children}
+    </ButtonWrapper>
+  );
+}
+
+Button.propTypes = {
+  buttonWidth: PropTypes.number,
+  buttonHeight: PropTypes.number,
+};
+
+export default Button;
