e8751f2cb3934e7629a202d3fbc70d3079c35682
Sunjae95 <tjswo9502@gmail.com>
2022-01-11 14:10:02 +0900

    feat: LinkText, Badge

--- src/components/LinkText.js
+++ src/components/LinkText.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 const LinkTextWrapper = styled.a`
+  ${({ isMain }) =>
+    isMain &&
+    css`
+      display: inline-block;
+      position: relative;
+      vertical-align: middle;
+      line-height: 20px;
+      padding: 15px;
+    `}
   ${({ bold }) => bold && `font-weight: 600`};
   font-size: ${({ textSize }) => `${textSize}px`};
 `;
 
-const LinkText = ({ children, bold, textSize, ...props }) => {
+const LinkText = ({ children, isMain, bold, textSize, ...props }) => {
   return (
-    <LinkTextWrapper bold={bold} textSize={textSize} {...props}>
+    <LinkTextWrapper isMain={isMain} bold={bold} textSize={textSize} {...props}>
       {children}
     </LinkTextWrapper>
   );
 };
 
 LinkText.propTypes = {
+  isMain: PropTypes.bool,
   bold: PropTypes.bool,
   textSize: PropTypes.number,
 };
