328d8f57f53c5e7336a0a99b059dc0abcf0ecfa3
Sunjae95 <tjswo9502@gmail.com>
2022-01-19 09:59:46 +0900

    feat: Padding 컴포넌트

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,3 +1,4 @@
 export { default as Badge } from './Badge';
 export { default as IconButton } from './IconButton';
 export { default as ListItem } from './ListItem';
+export { default as Padding } from './Padding';
